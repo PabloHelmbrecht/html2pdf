efbac0166fdd4e8c12ce94c79eb5c4b6b82aeca7
pablohelmbrecht <pablohelmbrecht@gmail.com>
2024-03-11 17:23:34 +0100

    link test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -142,13 +142,3 @@ export default function Dossier() {
   );
 }
 
-
-// This gets called on every request
-export async function getServerSideProps() {
-  // Fetch data from external API
-  const res = await fetch(`https://.../data`)
-  const data = await res.json()
- 
-  // Pass data to the page via props
-  return { props: { data } }
-}
